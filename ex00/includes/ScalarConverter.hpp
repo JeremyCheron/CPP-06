b3754d5a5ab59ba9c6bf1246e4714925fd365aa5
Jeremy Cheron <jcheron@student.42lehavre.fr>
2025-04-08 07:46:51 +0200

    cleaning repo

--- ex00/includes/ScalarConverter.hpp
+++ ex00/includes/ScalarConverter.hpp
@@ -11,25 +11,25 @@
 
 class ScalarConverter {
 private:
-    ScalarConverter();
-    ScalarConverter(const ScalarConverter& other);
-    ScalarConverter& operator=(const ScalarConverter& other);
-    ~ScalarConverter();
+	ScalarConverter();
+	ScalarConverter(const ScalarConverter& other);
+	ScalarConverter& operator=(const ScalarConverter& other);
+	~ScalarConverter();
 
-    static bool isChar(const std::string& str);
-    static bool isInt(const std::string& str);
-    static bool isFloat(const std::string& str);
-    static bool isDouble(const std::string& str);
-    static bool isSpecial(const std::string& str);
+	static bool isChar(const std::string& str);
+	static bool isInt(const std::string& str);
+	static bool isFloat(const std::string& str);
+	static bool isDouble(const std::string& str);
+	static bool isSpecial(const std::string& str);
 
-    static void printChar(char c);
-    static void printInt(int i);
-    static void printFloat(float f);
-    static void printDouble(double d);
-    static void printSpecial(const std::string& str);
+	static void printChar(char c);
+	static void printInt(int i);
+	static void printFloat(float f);
+	static void printDouble(double d);
+	static void printSpecial(const std::string& str);
 
 public:
-    static void convert(const std::string& str);
+	static void convert(const std::string& str);
 };
 
-#endif 
+#endif
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,11 +1,11 @@
 #include "../includes/ScalarConverter.hpp"
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cout << "Usage: ./convert <value>" << std::endl;
-        return 1;
-    }
+	if (argc != 2) {
+		std::cout << "Usage: ./convert <value>" << std::endl;
+		return 1;
+	}
 
-    ScalarConverter::convert(argv[1]);
-    return 0;
-} 
+	ScalarConverter::convert(argv[1]);
+	return 0;
+}
